9491058a821bc0fe4e2ecd0d54a3c20ef4049796
Branson Smith <bransonsmith22@gmail.com>
2021-05-28 10:57:40 -0500

    Add Action Select funcitonality

--- src/components/ActionSelect/ActionSelect.js
+++ src/components/ActionSelect/ActionSelect.js
@@ -3,19 +3,31 @@ import './ActionSelect.css';
 
 export default class ActionSelect extends React.Component  {
     
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Select a ' + this.props.label
+    };
+
+    this.selectionMade = this.selectionMade.bind(this);
+  }
+
+  selectionMade(event) {
+    this.setState({value: event.target.value})
+    this.props.onSelection(this.props.label, event.target.value);
+  }
+
   render(){
     return(
-      <select className="action-select">
-          <option>Action Select to be implemented</option>
-      </select>
-
-
-        /* <select>
-            {this.state.location_types.map((value, index) => {
-              return <option key={index}> { value.name } </option>
-            })}
+      <div className="action-select">
+        <label>{this.props.label}</label>
+        <select value={this.state.value} className="select-component" onChange={this.selectionMade} placeholder="Select a Roll Table">
+          <option value='Select a Roll Table' disabled>Select a Roll Table</option>
+          {this.props.values.map((value, index) => {
+            return <option key={index}>{value.name}</option>
+          })}
         </select>
-        <label>Location Type</label> */
+      </div>
     );
   }
 }
